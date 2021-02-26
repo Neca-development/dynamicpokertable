550c7e0043ad1888a628d11759150049d5a82d5d
Ernest <33957800+ErnestMironov@users.noreply.github.com>
2021-02-26 11:10:47 +0300

    add players grid

--- dynamicpockertable.js
+++ dynamicpockertable.js
@@ -1,27 +1,29 @@
-function drawTable (players){
+function drawTable(players) {
   var $tablewrapper = document.getElementById('dynamicpockertable');
 
   $tablewrapper.innerHTML = `
     <div class="table-wrapper">
       <div class="table" id="table"></div>
     </div>
-    <div class="message-box" id="message-box"></div>
-  `;
+    <div class="players-grid players-grid_2-player">
+    </div >
+  <div class="message-box" id="message-box"></div>
+`;
 
   sendMessageToTable(`Hello from container!!!`);
 }
 
-function applyColorsToTable(tableColor, borderColor){
+function applyColorsToTable(tableColor, borderColor) {
   var $table = document.getElementById('table');
-  
+
   $table.style.backgroundColor = tableColor;
   $table.style.borderColor = borderColor;
 }
 
-function sendMessageToTable(message){
+function sendMessageToTable(message) {
   var $tablecontainer = document.getElementById('message-box');
   $tablecontainer.innerHTML = message;
 }
 
 drawTable([]);
-applyColorsToTable( '#076B94', '#666');
+applyColorsToTable('#076B94', '#666');
