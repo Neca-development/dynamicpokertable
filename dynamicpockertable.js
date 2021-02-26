e830338003dffbb7a464fa7a9e570d53315db82b
Ernest <33957800+ErnestMironov@users.noreply.github.com>
2021-02-26 12:40:28 +0300

    добавилл функцию обновления доски

--- dynamicpockertable.js
+++ dynamicpockertable.js
@@ -5,12 +5,30 @@ function drawTable(players) {
     <div class="table-wrapper">
       <div class="table" id="table"></div>
     </div>
-    <div class="players-grid players-grid_2-player">
-    </div >
-  <div class="message-box" id="message-box"></div>
+    <div class="message-box" id="message-box"></div>
+    <div class="players-grid"></div >
 `;
-
   sendMessageToTable(`Hello from container!!!`);
+}
+
+function addPlayer(name, color, country, coins) {
+  const grid = document.querySelector('.players-grid'),
+    player = document.createElement('div')
+  id = Date.now()
+
+  player.setAttribute('id', id)
+  player.classList.add('player')
+
+  grid.append(player)
+  refreshPlayersGrid()
+}
+
+function refreshPlayersGrid() {
+  const grid = document.querySelector('.players-grid'),
+    playersCount = document.querySelectorAll('.players-grid .player').length;
+
+  grid.classList.remove(grid.classList.item(1))
+  grid.classList.add(`players-grid_${playersCount}-players`)
 }
 
 function applyColorsToTable(tableColor, borderColor) {
